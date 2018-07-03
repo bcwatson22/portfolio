c9a680931be705a0d7b795e3faa92be6e3e0fec6
Billy Watson <watson@Snowmen.home>
2018-07-03 11:41:55 +0100

    light and dark svgs, icon styling with hover etc, moved modal css into comp file

--- src/components/_common/Modal.vue
+++ src/components/_common/Modal.vue
@@ -61,5 +61,87 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
+  .modal {
+    .mask {
+      position: fixed;
+      z-index: 10;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      padding: 30px;
+      background-color: rgba(0, 0, 0, 0.9);
+      display: flex;
+      justify-content: space-around;
 
+      .container {
+        overflow: hidden;
+        will-change: opacity, transform;
+
+        .project-image {
+          height: 100%;
+          overflow-y: scroll;
+        }
+
+        .project-info {
+          position: fixed;
+          bottom: 0;
+          left: 0;
+          padding: 30px 30% 30px 30px;
+          background-image: linear-gradient(to bottom, transparent, rgba(0, 0, 0, 0.6) 50%, rgba(0, 0, 0, 0.6) 100%);
+          opacity: 0;
+          transform: translateY(100%);
+          will-change: opacity, transform;
+          transition: all 0.3s ease;
+
+          &.visible {
+            opacity: 1;
+            transform: translateY(0%);
+          }
+
+          h2 {
+            margin-top: 30px;
+          }
+
+          p {
+            margin-bottom: 0;
+          }
+        }
+
+        .close,
+        .toggle {
+          position: fixed;
+          right: 30px;
+        }
+
+        .close {
+          top: 30px;
+
+          &:before {
+            background: url('./../../assets/images/global/icons/close-light.svg') 50% 50% no-repeat;
+            background-size: 50% 50%;
+          }
+
+          &:after {
+            background: url('./../../assets/images/global/icons/close-dark.svg') 50% 50% no-repeat;
+            background-size: 50% 50%;
+          }
+        }
+
+        .toggle {
+          bottom: 30px;
+
+          &:before {
+            background: url('./../../assets/images/global/icons/info-light.svg') 50% 50% no-repeat;
+            background-size: 50% 50%;
+          }
+
+          &:after {
+            background: url('./../../assets/images/global/icons/info-dark.svg') 50% 50% no-repeat;
+            background-size: 50% 50%;
+          }
+        }
+      }
+    }
+  }
 </style>
