f96e187e57f290de6bb3e18f54cfc1df3991525c
Billy Watson <watson@Snowmen.home>
2018-07-24 12:19:01 +0100

    added navigation guard to check filter. if non-existent, redirect to /projects

--- src/scripts/router.js
+++ src/scripts/router.js
@@ -6,7 +6,7 @@ import Contact from './../components/_common/Contact';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   routes: [
     {
@@ -40,4 +40,35 @@ export default new Router({
       redirect: '/projects'
     }
   ]
-})
+});
+
+router.beforeEach((to, from, next) => {
+
+    let newFilter = to.query.filter;
+
+    if (newFilter) {
+
+      switch (newFilter) {
+
+        case 'development':
+        case 'design':
+        case 'branding':
+        case 'video':
+          next();
+          break;
+
+        default:
+          next('/');
+          break;
+
+      }
+
+    } else {
+
+      next();
+
+    }
+
+});
+
+export default router;
